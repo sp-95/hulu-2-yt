e66cc08cf1259b51c72813d4c011665b86557a29
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-15 14:20:41 +0545

    Feat: Place hulu logo with an appropriate background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header: React.FunctionComponent = () => {
   return (
     <header className="">
       <h1>This is the header</h1>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
     </header>
   )
 }
