b83f43e2bcd526aaeb8ab4011ac8754b84520cee
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-15 16:49:08 +0545

    Feat: Perform query on clicking the nav buttons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,22 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 import requests from '../utils/requests'
 
 const Nav: React.FunctionComponent = () => {
+  const router = useRouter()
+
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title }]) => (
-          <h2
+          <button
+            type="button"
             key={key}
-            className="cursor-pointer transition duration-100 transform hover:scale-125 active:text-red-500"
+            onClick={() => router.push(`/?genre=${key}`)}
+            className="focus:outline-none transition duration-100 transform hover:scale-125 active:text-red-500"
           >
             {title}
-          </h2>
+          </button>
         ))}
       </div>
       <div className="absolute top-0 left-0 bg-gradient-to-r from-primary h-10 w-1/12" />
