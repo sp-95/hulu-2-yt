0a36ac6c5bbabdd1d09365b1e231873ef55641f6
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-15 14:16:41 +0545

    Feat: Initialize header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <header className="">
+      <h1>This is the header</h1>
+    </header>
+  )
+}
+
+export default Header
